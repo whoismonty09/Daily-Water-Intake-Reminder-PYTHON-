b24fb52eafb86c8f3924aad2c07197d014c77a0f
Mayank malviya <mayankmalviya440@gmail.com>
2026-02-27 22:53:30 +0530

    Update WaterReminder.py

--- WaterReminder.py
+++ WaterReminder.py
@@ -1,1 +1,15 @@
+import time
+import winsound
+
+print("Water Reminder developed by Monty")
+interval = int(input("Enter reminder interval in minutes: "))
+
+print("Water Reminder Started Press Ctrl C to stop")
+
+while True:
+    time.sleep(interval * 60)
+    print("Time to drink water Stay hydrated")
+    winsound.Beep(1000, 1000)
+
+
 
